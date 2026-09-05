db3a4cb83fbd4eea263c1371411152805fa96983
Chindarat <buytaxusa@gmail.com>
2026-09-05 12:43:44 +0700

    feat: initialize project directory structure and core URL configuration

--- zonein/urls.py
+++ zonein/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("web.urls")),
 ]
 
-handler500 = "web.views.custom_500"
